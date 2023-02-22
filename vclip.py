dbccdc32ed386c1380b7ced770f1d5fb6ff9dbea
ldiv <ldiv@ml1.net>
2023-02-21 21:42:35 -0500

    Refactor
    - Allow end time optional (clip until the end of the source file)
    - Simplify validation of time intervals by leveraging argparse type param
    - Use pathlib instead of os.path
    - Cleanup

--- vclip.py
+++ vclip.py
@@ -1,21 +1,25 @@
-import os
-import sys
 import argparse
-import subprocess
-import tempfile
+import os
+import pathlib
 import shutil
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+from typing import List
 
 
 DEFAULT_OUTPUT_PATH = os.getcwd()
-FFMPEG_BINARY = "/usr/bin/ffmpeg"
+FFMPEG_BINARY = os.getenv("FFMPEG_PATH")
 
 
-def ffmpeg_extract_clip(filename, start_time, end_time, output_path):
-    cmd = [FFMPEG_BINARY, "-i", filename, "-y",
-           "-ss", "{:0.2f}".format(start_time), "-t", "{:0.2f}".format(end_time - start_time),
-           "-async", "1", "-strict", "-2", output_path]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return proc
+def ffmpeg_extract_clip(input_path, output_path, start_time, end_time=None):
+    cmd = [FFMPEG_BINARY, "-i", input_path, "-y", "-ss", "{:0.2f}".format(start_time)]
+    if end_time:
+        cmd.extend(["-t", "{:0.2f}".format(end_time - start_time)])
+    cmd.extend(["-async", "1", "-strict", "-2", output_path])
+
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def ffmpeg_merge_clips(clips, output_path):
@@ -46,7 +50,7 @@ class TimePoint:
     def __init__(self, time_endpoint_string):
         time_sections = time_endpoint_string.split(":")
         if len(time_sections) < 1 or len(time_sections) > 3:
-            raise InvalidTimeIntervalException("Invalid string: must contain at least minutes and seconds (MM:SS)")
+            raise InvalidTimeIntervalException("Invalid string: must contain at least minutes and seconds ([M]M:SS)")
         # A seconds value is required
         self.seconds = TimePoint.parse_time_section(time_sections[-1])
         # Minutes and hours are optional so initialize to zero then set if present
@@ -60,17 +64,14 @@ class TimePoint:
     def convert_to_seconds(self):
         return self.seconds + (self.minutes * 60) + (self.hours * 60 * 60)
 
-    def __repr__(self):
-        return "{}:{}:{}".format(self.hours, self.minutes, self.seconds)
-
     @staticmethod
     def parse_time_section(time_section_string, max_=60):
         try:
             time_duration = int(time_section_string)
         except ValueError:
-            raise InvalidTimeDurationException("time duration ({}) value not a number".format(time_section_string))
-        if time_duration > max_:
-            raise InvalidTimeDurationException("time duration value exceeds maximum ({})".format(max_))
+            raise InvalidTimeDurationException(f"Value ({time_section_string}) is not a number")
+        if not (0 <= time_duration <= max_):
+            raise InvalidTimeDurationException(f"Value must be between 0 and ({max_})")
         return time_duration
 
     def __lt__(self, other_time_point):
@@ -92,33 +93,25 @@ class TimePoint:
         return "{:02d}:{:02d}:{:02d}".format(self.hours, self.minutes, self.seconds)
 
 
-class TimePointEnd:
-    def __init__(self):
-        self.seconds = None
-        self.minutes = None
-        self.hours = None
-
-    def convert_to_seconds(self):
-        return None
-
-    def __repr__(self):
-        return "End of Video"
-
-
 class TimeInterval:
-    def __init__(self, time_interval_string):
-        try:
+    def __init__(self, time_interval_string: str):
+        if "-" in time_interval_string:
             start_time, end_time = time_interval_string.split("-")
-        except ValueError:
-            raise InvalidTimeIntervalException("not a valid time interval")
+        else:
+            start_time = time_interval_string
+            end_time = None
 
         self.start_time = TimePoint(start_time)
-        if end_time == "end":
-            self.end_time = TimePointEnd()
-        else:
-            self.end_time = TimePoint(end_time)
+        self.end_time = TimePoint(end_time) if end_time else None
+
+    def __len__(self):
+        if self.end_time:
+            return self.end_time.convert_to_seconds() - self.start_time.convert_to_seconds()
+        return 0
 
     def __repr__(self):
+        if not self.end_time:
+            return "{}".format(self.start_time)
         return "{}-{}".format(self.start_time, self.end_time)
 
 
@@ -129,8 +122,7 @@ def validate_intervals(intervals):
         while current_interval_index < len(intervals)-1:
             current_end = intervals[current_interval_index].end_time
             next_start = intervals[current_interval_index+1].start_time
-            print("VALIDATING {} {}".format(current_end, next_start))
-            if current_end >= next_start:
+            if current_end and current_end >= next_start:
                 raise InvalidTimeIntervalException("Time intervals overlap")
             current_interval_index += 1
 
@@ -145,20 +137,26 @@ def read_proc_stdout(proc, message=None):
             break
 
 
-def clip_video(filename, intervals, output_path):
-    intervals = [TimeInterval(interval_string) for interval_string in intervals]
-    # Sort by start time to ensure intervals are in chronological order
+def clip_video(filename: Path, intervals: List[TimeInterval], output_path: Path):
     intervals = sorted(intervals, key=lambda t: t.start_time.convert_to_seconds())
     print("Intervals to clip {}".format(intervals))
+
     validate_intervals(intervals)
+
     tmpdir = tempfile.TemporaryDirectory()
     clips = []
     for i, interval in enumerate(intervals):
-        clip_filename = os.path.join(tmpdir.name, "tmp{}.mp4".format(i))
-        clips.append(clip_filename)
-        proc = ffmpeg_extract_clip(filename, interval.start_time.convert_to_seconds(),
-                                   interval.end_time.convert_to_seconds(), clip_filename)
+        clip_filename = Path(f"{tmpdir.name}/tmp{i}{filename.suffix}")
+        clips.append(clip_filename.absolute())
+        proc = ffmpeg_extract_clip(
+            filename,
+            clip_filename,
+            interval.start_time.convert_to_seconds(),
+            interval.end_time.convert_to_seconds() if interval.end_time else None
+        )
         read_proc_stdout(proc, "Processing subclip {} out of {}".format(i+1, len(intervals)))
+    # If there are multiple clips, merge them
+    # TODO: should make this optional (--merge defaulting to True)
     if len(clips) > 1:
         proc = ffmpeg_merge_clips(clips, output_path)
         read_proc_stdout(proc, "Merging subclips")
@@ -171,45 +169,36 @@ def display_usage():
     return """
         python3 vclip.py input_video_file_name interval [intervals ...]
 
-        interval format: [HH:]MM:SS-[HH:]MM:SS
-                         [HH:]MM:SS-end
+        interval format: [HH:][M]M:SS-[HH:][M]M:SS
+                         [HH:][M]M:SS
     """
 
 
-def check_file_exists(filepath):
-    if not os.path.isfile(filepath):
-        print("Invalid file path: {}".format(filepath))
-        sys.exit(-1)
-
-
 def get_input_path(file_path_arg):
-    return os.path.abspath(file_path_arg)
+    input_path = Path(file_path_arg).absolute()
+    if not input_path.exists():
+        raise InputFileNotFoundException(f"Invalid path: {input_path}")
+    return input_path
 
 
-def get_output_path(input_path):
-    filename = os.path.basename(input_path)
-    output_dir = DEFAULT_OUTPUT_PATH
-    output_file = filename.lower().replace(" ", "_").replace(".mp4", "_clip.mp4")
-    return os.path.join(output_dir, output_file)
+def get_output_path(input_path, output_path=DEFAULT_OUTPUT_PATH):
+    filename = Path(input_path)
+    output_dir = Path(output_path)
+    extension = filename.suffix
+    output_file = filename.name.lower().replace(" ", "_").replace(extension, f"_clip{extension}")
+    return Path(f"{output_dir.name}/{output_file}").absolute()
+
+
+def intervals_argument(interval_argument: str) -> TimeInterval:
+    return TimeInterval(interval_argument)
 
 
 def process_arguments(args):
-    if not args.input_file:
-        print("should error before getting here")
-        print(display_usage())
-        return False
-    if args.interval_file and args.intervals:
-        #TODO: pick one to override and just print a warning
-        print("Can only accept time interval parameters via command line arguments or file, not both")
-        print(display_usage())
-        return False
     if args.intervals:
         input_path = get_input_path(args.input_file)
-        output_path = get_output_path(input_path)
-        check_file_exists(input_path)
+        output_path = get_output_path(input_path, args.output_file)
         clip_video(input_path, args.intervals, output_path)
     elif args.interval_file:
-        print("checking that {} is a legit file and has intervals".format(args.interval_file))
         return "Not supported yet"
     else:
         print("No interval parameters were passed, or intervals file found for {}".format(args.input_file))
@@ -218,38 +207,61 @@ def process_arguments(args):
 
 def create_arg_parser():
     arg_parser = argparse.ArgumentParser()
-    # The only positional argument is the file name of the source video file to clip
     arg_parser.add_argument("input_file", help="video file to clip")
-    arg_parser.add_argument("-i", "--intervals", nargs="*",
-                            help="intervals [HH:]MM:SS-[HH:]MM:SS [[HH:]MM:SS-[HH:]MM:SS]")
-    arg_parser.add_argument("-f", "--interval_file",
-                            help="file containing one interval per line")
-    arg_parser.add_argument("-o", "--output_file",
+    group = arg_parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-i",
+        "--intervals",
+        nargs="+",
+        type=intervals_argument,
+        help="intervals [HH:]MM:SS-[HH:]MM:SS [[HH:]MM:SS-[HH:]MM:SS]"
+    )
+    group.add_argument("-f", "--interval_file", nargs="?", help="file containing one interval per line")
+    arg_parser.add_argument("-o", "--output_file", nargs="?", default=".",
                             help="file path for the resulting video file")
     return arg_parser
 
 
-def run():
-    arg_parser = create_arg_parser()
-    process_arguments(arg_parser.parse_args())
-
-
-class CustomException(Exception):
-    def __init__(self, msg):
-        print("Error: {}".format(msg))
+class ApplicationException(Exception):
+    def __init__(self, msg=None):
+        print(f"{self.__class__.__name__} {'- ' + msg if msg else ''}")
         sys.exit(1)
 
 
-class WrongParametersException(CustomException):
+class InputFileNotFoundException(ApplicationException):
     pass
 
 
-class InvalidTimeDurationException(CustomException):
+class WrongParametersException(ApplicationException):
     pass
 
 
-class InvalidTimeIntervalException(CustomException):
+class InvalidTimeDurationException(ApplicationException):
     pass
+
+
+class InvalidTimeIntervalException(ApplicationException):
+    pass
+
+
+class MissingFfmpegLocation(ApplicationException):
+    pass
+
+
+def _validate_ffmpeg_path():
+    if FFMPEG_BINARY and pathlib.Path(FFMPEG_BINARY).exists():
+        return True
+    raise MissingFfmpegLocation
+
+
+def run():
+    _validate_ffmpeg_path()
+    arg_parser = create_arg_parser()
+    try:
+        process_arguments(arg_parser.parse_args())
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
